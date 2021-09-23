56ba2e121e6488d02eac9faf931618fb6ae35611
Parker Meinershagen <pkmeiner@gmail.com>
2021-09-23 09:32:08 -0500

    Lots of updates
    added soft-ban, fixed locking, and tried to get warnings work, but failed so I deleted everything.

--- bot.py
+++ bot.py
@@ -3,20 +3,12 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import MissingPermissions
 
-bot = commands.Bot(command_prefix = "!")
-
 log_channel_id = 889293946801516554
+
+bot = commands.Bot(command_prefix = "!")
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
-
-
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[:-3]}')
-
-
-
-
+                
 bot.run("ODg5MDI3MTI1Mjc1OTIyNDYy.YUbRBw.96QzGKIyYjyIwKNuXG-w2-i73rY")
--- cogs/moderation.py
+++ cogs/moderation.py
@@ -3,8 +3,13 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import MissingPermissions
 from discord.utils import get
+import datetime
+import csv 
 
+timestamp=str(datetime.datetime.utcnow())
 log_channel_id = 889293946801516554
+
+yellow = 0xffc400
 
 class moderation(commands.Cog):
 
@@ -13,51 +18,117 @@ class moderation(commands.Cog):
 
 
     @commands.command()
+    async def test(self, ctx):
+        message = ctx.message
+        await ctx.send('Command issued at: ' + message.created_at.strftime('%Y-%m-%d %H:%M:%S %Z%z'))
+
+
+
+
+
+    @commands.command()
     @commands.has_permissions(kick_members=True)
     async def kick(self, ctx, member: discord.Member, *, reason=None):
         log = self.bot.get_channel(log_channel_id)
+        message = ctx.message
 
-        embed = discord.Embed(
-            title = f"**User {member} has been kicked for {reason}**",
-            colour = discord.Colour.orange()
-        )
+        if reason == None:
+            await ctx.send(f"{ctx.author.mention}, you must provide a reason in order to kick {member}")
+        
+        elif reason != None:
+            embed = discord.Embed(
+                title = f"**User {member} has been kicked for {reason}**",
+                colour = yellow
+            )
 
-        embed.add_field(name=f'This command was issued by {ctx.author}', value = f'This has been logged to {log.mention}', inline=False)
-        embed.set_thumbnail(url = member.avatar_url)
-        embed.set_footer(text='Bot made by Fiji#3608')
+            embed.add_field(name=f'This command was issued by {ctx.author}', value = f'This has been logged to {log.mention}', inline=False)
+            embed.set_thumbnail(url = member.avatar_url)
+            embed.set_footer(text='Command issued at: ' + message.created_at.strftime('%Y-%m-%d %H:%M:%S %Z%z'))
 
-        await ctx.send(embed=embed)
-        await member.kick(reason=reason)
+            await ctx.send(embed=embed)
+            await member.kick(reason=reason)
 
 
     @commands.command()
     @commands.has_permissions(kick_members=True)
     async def ban(self, ctx, member: discord.Member, *, reason=None):
         log = self.bot.get_channel(log_channel_id)
+        message = ctx.message
 
-        embed = discord.Embed(
-            title = f"**User {member} has been banned for {reason}**",
-            colour = discord.Colour.red()
-        )
+        if reason == None:
+            await ctx.send(f"{ctx.author.mention}, you must provide a reason in order to ban {member}")
 
-        embed.add_field(name=f'This command was issued by {ctx.author}', value = f'This has been logged to {log.mention}', inline=False)
-        embed.set_thumbnail(url = member.avatar_url)
-        embed.set_footer(text='Bot made by Fiji#3608')
+        elif reason != None:
+            embed = discord.Embed(
+                title = f"**User {member} has been banned for {reason}**",
+                colour = discord.Colour.red()
+            )
 
-        await ctx.send(embed=embed)
-        await member.kick(reason=reason)
+            embed.add_field(name=f'This command was issued by {ctx.author}', value = f'This has been logged to {log.mention}', inline=False)
+            embed.set_thumbnail(url = member.avatar_url)
+            embed.set_footer(text='Command issued at: ' + message.created_at.strftime('%Y-%m-%d %H:%M:%S %Z%z'))
+
+            await ctx.send(embed=embed)
+            await member.kick(reason=reason)
+
+
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def softban(self, ctx, member: discord.Member, *, reason=None):
+        log = self.bot.get_channel(log_channel_id)
+        message = ctx.message
+
+        if reason == None:
+            embed = discord.Embed(
+                title = f"**User {member} has been soft-banned with no given reason**",
+                colour = discord.Colour.orange()
+            )
+
+            embed.add_field(name=f'This command was issued by {ctx.author}', value = f'This has been logged to {log.mention}', inline=False)
+            embed.set_thumbnail(url = member.avatar_url)
+            embed.set_footer(text='Command issued at: ' + message.created_at.strftime('%Y-%m-%d %H:%M:%S %Z%z'))
+
+            await ctx.send(embed=embed)
+            await member.ban(reason=reason)
+            await member.unban()
+
+        elif reason != None:
+            embed = discord.Embed(
+                title = f"**User {member} has been soft-banned for {reason}**",
+                colour = discord.Colour.orange()
+            )
+
+            embed.add_field(name=f'This command was issued by {ctx.author}', value = f'This has been logged to {log.mention}', inline=False)
+            embed.set_thumbnail(url = member.avatar_url)
+            embed.set_footer(text='Command issued at: ' + message.created_at.strftime('%Y-%m-%d %H:%M:%S %Z%z'))
+
+            await ctx.send(embed=embed)
+            await member.kick(reason=reason)
 
 
     @commands.command()
     @commands.has_permissions(kick_members=True)
     async def deafen(self, ctx, member: discord.Member, *, reason=None):
         log = self.bot.get_channel(log_channel_id)
-        voice_state = member.voice
+        message = ctx.message
 
         if member.voice is None:
             await ctx.send(f'{ctx.author.mention}, that member is not in a voice channel and therefore cannot be deafened.')
 
-        else:
+        elif member.voice != None and reason == None:
+            embed = discord.Embed(
+                title = f"**User {member} has been deafened with no given reason.**",
+                colour = discord.Colour.magenta()
+            )
+
+            embed.add_field(name=f'This command was issued by {ctx.author}', value = f'This has been logged to {log.mention}', inline=False)
+            embed.set_thumbnail(url = member.avatar_url)
+            embed.set_footer(text='Command issued at: ' + message.created_at.strftime('%Y-%m-%d %H:%M:%S %Z%z'))
+
+            await ctx.send(embed=embed)
+            await member.edit(deafen=True)
+
+        elif member.voice != None and reason != None:
             embed = discord.Embed(
                 title = f"**User {member} has been deafened for {reason}**",
                 colour = discord.Colour.magenta()
@@ -65,7 +136,7 @@ class moderation(commands.Cog):
 
             embed.add_field(name=f'This command was issued by {ctx.author}', value = f'This has been logged to {log.mention}', inline=False)
             embed.set_thumbnail(url = member.avatar_url)
-            embed.set_footer(text='Bot made by Fiji#3608')
+            embed.set_footer(text='Command issued at: ' + message.created_at.strftime('%Y-%m-%d %H:%M:%S %Z%z'))
 
             await ctx.send(embed=embed)
             await member.edit(deafen=True)
@@ -75,12 +146,25 @@ class moderation(commands.Cog):
     @commands.has_permissions(kick_members=True)
     async def undeafen(self, ctx, member: discord.Member, *, reason=None):
         log = self.bot.get_channel(log_channel_id)
-        voice_state = member.voice
+        message = ctx.message
 
         if member.voice is None:
             await ctx.send(f'{ctx.author.mention}, that member is not in a voice channel and therefore cannot be undeafened.')
 
-        else:
+        elif member.voice != None and reason == None:
+            embed = discord.Embed(
+                title = f"**User {member} has been undeafened with no given reason**",
+                colour = discord.Colour.magenta()
+            )
+
+            embed.add_field(name=f'This command was issued by {ctx.author}', value = f'This has been logged to {log.mention}', inline=False)
+            embed.set_thumbnail(url = member.avatar_url)
+            embed.set_footer(text='Command issued at: ' + message.created_at.strftime('%Y-%m-%d %H:%M:%S %Z%z'))
+
+            await ctx.send(embed=embed)
+            await member.edit(deafen=False)
+
+        elif member.voice != None and reason != None:
             embed = discord.Embed(
                 title = f"**User {member} has been undeafened for {reason}**",
                 colour = discord.Colour.magenta()
@@ -88,56 +172,22 @@ class moderation(commands.Cog):
 
             embed.add_field(name=f'This command was issued by {ctx.author}', value = f'This has been logged to {log.mention}', inline=False)
             embed.set_thumbnail(url = member.avatar_url)
-            embed.set_footer(text='Bot made by Fiji#3608')
+            embed.set_footer(text='Command issued at: ' + message.created_at.strftime('%Y-%m-%d %H:%M:%S %Z%z'))
 
             await ctx.send(embed=embed)
             await member.edit(deafen=False)
 
 
-    @commands.command()
-    @commands.has_permissions(kick_members=True)
-    async def mute(self, ctx, member: discord.Member, *, reason=None):
-        log = self.bot.get_channel(log_channel_id)
-        voice_state = member.voice
-
-        if member.voice is None:
-            await ctx.send(f'{ctx.author.mention}, that member is not in a voice channel and therefore cannot be muted.')
-
-        else:
-            embed = discord.Embed(
-                title = f"**User {member} has been muted for {reason}**",
-                colour = discord.Colour.magenta()
-            )
-
-            embed.add_field(name=f'This command was issued by {ctx.author}', value = f'This has been logged to {log.mention}', inline=False)
-            embed.set_thumbnail(url = member.avatar_url)
-            embed.set_footer(text='Bot made by Fiji#3608')
-
-            await ctx.send(embed=embed)
-            await member.edit(mute=True)
+    #@commands.command()
+    #@commands.has_permissions(kick_members=True)
+    #async def mute(self, ctx, member: discord.Member, *, reason=None):
+#Give user the muted role 
 
 
-    @commands.command()
-    @commands.has_permissions(kick_members=True)
-    async def unmute(self, ctx, member: discord.Member, *, reason=None):
-        log = self.bot.get_channel(log_channel_id)
-        voice_state = member.voice
-
-        if member.voice is None:
-            await ctx.send(f'{ctx.author.mention}, that member is not in a voice channel and therefore cannot be unmuted.')
-
-        else:
-            embed = discord.Embed(
-                title = f"**User {member} has been unmuted for {reason}**",
-                colour = discord.Colour.magenta()
-            )
-
-            embed.add_field(name=f'This command was issued by {ctx.author}', value = f'This has been logged to {log.mention}', inline=False)
-            embed.set_thumbnail(url = member.avatar_url)
-            embed.set_footer(text='Bot made by Fiji#3608')
-
-            await ctx.send(embed=embed)
-            await member.edit(mute=False)
+    #@commands.command()
+    #@commands.has_permissions(kick_members=True)
+    #async def unmute(self, ctx, member: discord.Member, *, reason=None):
+#Takes muted role away from user
 
 
     @commands.command()
@@ -145,22 +195,49 @@ class moderation(commands.Cog):
     async def lock(self, ctx, channel: discord.TextChannel=None, *, reason=None):
         log = self.bot.get_channel(log_channel_id)
         channel = channel or ctx.channel
+        message = ctx.message
 
-        overwrites = {
-        ctx.guild.default_role: discord.PermissionOverwrite(send_messages=False)
-        }
-        await channel.edit(overwrites=overwrites)
-        embed = discord.Embed(
-            title = f"**Channel `{channel.name}` has been locked**",
-            description = f"Reason: {reason}",
-            colour = discord.Colour.magenta()
-        )
+        if reason == None:
+            cuties = discord.utils.get(ctx.guild.roles, name="cuties")
+            await channel.set_permissions(cuties, send_messages=False)
+            shout_ping = discord.utils.get(ctx.guild.roles, name="shout ping")
+            await channel.set_permissions(shout_ping, send_messages=False)
+            nitro_ping = discord.utils.get(ctx.guild.roles, name="nitro ping")
+            await channel.set_permissions(nitro_ping, send_messages=False)
+            event_ping = discord.utils.get(ctx.guild.roles, name="event ping")
+            await channel.set_permissions(event_ping, send_messages=False)
+            embed = discord.Embed(
+                title = f"**Channel `{channel.name}` has been locked**",
+                description = f"No given reason",
+                colour = discord.Colour.magenta()
+            )
 
-        embed.add_field(name=f'This command was issued by {ctx.author}', value = f'This has been logged to {log.mention}', inline=False)
-        embed.set_thumbnail(url="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/282/locked_1f512.png")
-        embed.set_footer(text='Bot made by Fiji#3608')
+            embed.add_field(name=f'This command was issued by {ctx.author}', value = f'This has been logged to {log.mention}', inline=False)
+            embed.set_thumbnail(url="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/282/locked_1f512.png")
+            embed.set_footer(text=f'Command issued at: ' + message.created_at.strftime('%Y-%m-%d %H:%M:%S %Z%z'))
 
-        await ctx.send(embed=embed)
+            await ctx.send(embed=embed)
+
+        if reason != None:
+            cuties = discord.utils.get(ctx.guild.roles, name="cuties")
+            await channel.set_permissions(cuties, send_messages=False)
+            shout_ping = discord.utils.get(ctx.guild.roles, name="shout ping")
+            await channel.set_permissions(shout_ping, send_messages=False)
+            nitro_ping = discord.utils.get(ctx.guild.roles, name="nitro ping")
+            await channel.set_permissions(nitro_ping, send_messages=False)
+            event_ping = discord.utils.get(ctx.guild.roles, name="event ping")
+            await channel.set_permissions(event_ping, send_messages=False)
+            embed = discord.Embed(
+                title = f"**Channel `{channel.name}` has been locked**",
+                description = f"Reason: {reason}",
+                colour = discord.Colour.magenta()
+            )
+
+            embed.add_field(name=f'This command was issued by {ctx.author}', value = f'This has been logged to {log.mention}', inline=False)
+            embed.set_thumbnail(url="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/282/locked_1f512.png")
+            embed.set_footer(text='Command issued at: ' + message.created_at.strftime('%Y-%m-%d %H:%M:%S %Z%z'))
+
+            await ctx.send(embed=embed)
 
 
     @commands.command()
@@ -168,22 +245,53 @@ class moderation(commands.Cog):
     async def unlock(self, ctx, channel: discord.TextChannel=None, *, reason=None):
         log = self.bot.get_channel(log_channel_id)
         channel = channel or ctx.channel
+        message = ctx.message
 
-        overwrites = {
-        ctx.guild.default_role: discord.PermissionOverwrite(send_messages=True)
-        }
-        await channel.edit(overwites=overwrites)
-        embed = discord.Embed(
-            title = f"**Channel `{channel.name}` has been unlocked**",
-            description = f"Reason: {reason}",
-            colour = discord.Colour.magenta()
-        )
-        embed.add_field(name=f'This command was issued by {ctx.author}', value = f'This has been logged to {log.mention}', inline=False)
-        embed.set_thumbnail(url="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/282/unlocked_1f513.png")
-        embed.set_footer(text='Bot made by Fiji#3608')
+        if reason == None:
+            cuties = discord.utils.get(ctx.guild.roles, name="cuties")
+            await channel.set_permissions(cuties, send_messages=True)
+            shout_ping = discord.utils.get(ctx.guild.roles, name="shout ping")
+            await channel.set_permissions(shout_ping, send_messages=True)
+            nitro_ping = discord.utils.get(ctx.guild.roles, name="nitro ping")
+            await channel.set_permissions(nitro_ping, send_messages=True)
+            event_ping = discord.utils.get(ctx.guild.roles, name="event ping")
+            await channel.set_permissions(event_ping, send_messages=True)
+            embed = discord.Embed(
+                title = f"**Channel `{channel.name}` has been unlocked**",
+                description = f"No given reason",
+                colour = discord.Colour.magenta()
+            )
+            embed.add_field(name=f'This command was issued by {ctx.author}', value = f'This has been logged to {log.mention}', inline=False)
+            embed.set_thumbnail(url="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/282/unlocked_1f513.png")
+            embed.set_footer(text='Command issued at: ' + message.created_at.strftime('%Y-%m-%d %H:%M:%S %Z%z'))
 
-        await ctx.send(embed=embed)
+            await ctx.send(embed=embed)
 
+        if reason != None:
+            cuties = discord.utils.get(ctx.guild.roles, name="cuties")
+            await channel.set_permissions(cuties, send_messages=True)
+            shout_ping = discord.utils.get(ctx.guild.roles, name="shout ping")
+            await channel.set_permissions(shout_ping, send_messages=True)
+            nitro_ping = discord.utils.get(ctx.guild.roles, name="nitro ping")
+            await channel.set_permissions(nitro_ping, send_messages=True)
+            event_ping = discord.utils.get(ctx.guild.roles, name="event ping")
+            await channel.set_permissions(event_ping, send_messages=True)
+            embed = discord.Embed(
+                title = f"**Channel `{channel.name}` has been unlocked**",
+                description = f"Reason: {reason}",
+                colour = discord.Colour.magenta()
+            )
+            embed.add_field(name=f'This command was issued by {ctx.author}', value = f'This has been logged to {log.mention}', inline=False)
+            embed.set_thumbnail(url="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/twitter/282/unlocked_1f513.png")
+            embed.set_footer(text='Command issued at: ' + message.created_at.strftime('%Y-%m-%d %H:%M:%S %Z%z'))
+
+            await ctx.send(embed=embed)
+
+
+
+
+
+            ##HERE STARTS THE WARN COMMANDS
 
 
 
