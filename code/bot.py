092ce98d03a2f5d3a4058258f92456fc524a521c
Parker Meinershagen <pkmeiner@gmail.com>
2021-10-07 18:20:51 -0500

    FINISHED
    COMPLETELY DONE, MOVE TO BETA BUILD

--- code/bot.py
+++ code/bot.py
@@ -16,6 +16,7 @@ bot = commands.Bot(
 @bot.event
 async def on_ready():
     print(f'{bot.user} has connected to nextcord!')
+    await bot.change_presence(activity = nextcord.Game("$help"))
 
 
 bot.remove_command('help')
---
+++ code/cogs/socketfix.py
@@ -0,0 +1,36 @@
+import zlib
+import nextcord
+
+from nextcord.ext import commands
+
+
+class SocketFix(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+        self._zlib = zlib.decompressobj()
+        self._buffer = bytearray()
+
+    @commands.Cog.listener()
+    async def on_socket_raw_receive(self, msg):
+        if type(msg) is bytes:
+            self._buffer.extend(msg)
+
+            if len(msg) < 4 or msg[-4:] != b'\x00\x00\xff\xff':
+                return
+
+            try:
+                msg = self._zlib.decompress(self._buffer)
+            except Exception:
+                self._buffer = bytearray()  # Reset buffer on fail just in case...
+                return
+
+            msg = msg.decode('utf-8')
+            self._buffer = bytearray()
+
+        msg = nextcord.utils._from_json(msg)
+        self.bot.dispatch('on_socket_response', msg)
+
+
+def setup(bot):
+    bot.add_cog(SocketFix(bot))
