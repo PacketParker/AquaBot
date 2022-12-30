1016f9720125eea8d2042a075edee00ac34e96f2
Parker <parker@pkrm.dev>
2022-12-30 00:39:18 -0600

    ***users -> ***info (more)

--- code/cogs/user_count.py
+++ code/cogs/user_count.py
@@ -1,4 +1,5 @@
 from discord.ext import commands
+import discord
 
 class UserCount(commands.Cog):
     def __init__(self, bot):
@@ -8,13 +9,30 @@ class UserCount(commands.Cog):
     @commands.command()
     @commands.dm_only()
     @commands.is_owner()
-    async def users(self, ctx: commands.Context):
-        members = 0
+    async def info(self, ctx: commands.Context):
+        total_guilds = {}
 
         for guild in self.bot.guilds:
-            members += guild.member_count
+            total_guilds[guild.name] = guild.member_count
 
-        await ctx.author.send(f"Total users: `{members:,}`")
+        # Sort the dictionary by value descending
+        total_guilds = dict(sorted(total_guilds.items(), key=lambda item: item[1], reverse=True))
+
+        total_members = 0
+
+        for guild in total_guilds:
+            total_members += total_guilds[guild]
+
+        embed = discord.Embed(
+            title="User Count",
+            description=f"Total Members: `{total_members:,}`\nTotal Guilds: `{len(self.bot.guilds):,}`",
+            color=discord.Color.blurple()
+        )
+        # Add the top 5 guilds to the embed
+        for guild in list(total_guilds)[:5]:
+            embed.add_field(name=guild, value=f"```{total_guilds[guild]:,}```", inline=False)
+
+        await ctx.send(embed=embed)
 
 
 async def setup(bot):
