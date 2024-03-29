4cdb9b5bcd0a4c506d74d2ce568e25498b1b40c4
Parker <contact@pkrm.dev>
2024-03-28 22:49:35 -0500

    Revert to older code

--- code/cogs/music.py
+++ code/cogs/music.py
@@ -3,12 +3,11 @@ import discord
 import lavalink
 from discord.ext import commands
 import math
+import requests
 import datetime
 from discord import app_commands
-from lavalink import LoadType, NodeConnectedEvent
-
-from global_variables import LAVALINK_HOST, LAVALINK_PASSWORD, LAVALINK_PORT, LOG, BOT_COLOR
-
+from custom_source import CustomSource
+from global_variables import BOT_COLOR
 
 url_rx = re.compile(r'https?://(?:www\.)?.+')
 
@@ -71,13 +70,15 @@ class Music(commands.Cog):
         if not hasattr(bot, 'lavalink'):  # This ensures the client isn't overwritten during cog reloads.
             bot.lavalink = lavalink.Client(self.bot.user.id)
             bot.lavalink.add_node(
-                LAVALINK_HOST,
-                LAVALINK_PORT,
-                LAVALINK_PASSWORD,
-                'us-central'
-            )  # Host, Port, Password, Region
+                '127.0.0.1',
+                2333,
+                'youshallnotpass',
+                'us-central',
+                'default-node'
+            )  # Host, Port, Password, Region, Name
+
         self.lavalink: lavalink.Client = bot.lavalink
-        self.lavalink.add_event_hooks(self)
+        self.lavalink.add_event_hooks(self.track_hook)
 
     def cog_unload(self):
         """ Cog unload handler. This removes any event hooks that were registered. """
@@ -119,20 +120,11 @@ class Music(commands.Cog):
                 await interaction.response.send_message('You need to be in my voicechannel.', ephemeral=True)
                 raise ZeroDivisionError
 
-
-    @lavalink.listener(lavalink.events.NodeConnectedEvent)
-    async def node_connected_event(self, event: lavalink.events.NodeConnectedEvent):
-        LOG.info(f"Connected to lavalink node {event.node.name}")
-
-
-    @lavalink.listener(lavalink.events.QueueEndEvent)
-    async def on_queue_end(self, event: lavalink.events.QueueEndEvent):
-        guild_id = event.player.guild_id
-        guild = self.bot.get_guild(guild_id)
-
-        if guild is not None:
+    async def track_hook(self, event):
+        if isinstance(event, lavalink.events.QueueEndEvent):
+            guild_id = event.player.guild_id
+            guild = self.bot.get_guild(guild_id)
             await guild.voice_client.disconnect(force=True)
-
 
     @commands.Cog.listener()
     async def on_voice_state_update(self, member, before, after):
@@ -157,37 +149,116 @@ class Music(commands.Cog):
         name: str
     ):
         "Play a song from your favorite music provider"
-        await self.ensure_voice(interaction)
+        await self.ensure_before(interaction)
         player = self.lavalink.player_manager.get(interaction.guild.id)
         query = name
 
+        # Below begins the start of the search for Spotify links - we must check for playlist, albums, and tracks
+        # We use a custom source in order to provide us with the correct information and streams
+        if "open.spotify.com/playlist" in query:
+            playlist_id = query.split("playlist/")[1].split("?si=")[0]
+            playlist_url = f"https://api.spotify.com/v1/playlists/{playlist_id}"
+            headers = {"Authorization": f"Bearer {self.bot.access_token}"}
+            response = requests.get(playlist_url, headers=headers)
+            if response.status_code == 200:
+                playlist = response.json()
+
+                embed = discord.Embed(
+                    title = "Playlist Queued",
+                    description = f"**{playlist['name']}** from **{playlist['owner']['display_name']}**\n` {len(playlist['tracks']['items'])} ` tracks\n\nQueued by: {interaction.user.mention}",
+                    color=BOT_COLOR
+                )
+                embed.set_thumbnail(url=playlist['images'][0]['url'])
+                embed.set_footer(text=datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%d %H:%M:%S')+" UTC")
+                await interaction.response.send_message(embed=embed)
+
+                tracks = await CustomSource.load_playlist(self, interaction.user, playlist)
+                for track in tracks['tracks']:
+                    player.add(requester=interaction.user, track=track)
+
+        if "open.spotify.com/album" in query:
+            album_id = query.split("album/")[1]
+            album_url = f"https://api.spotify.com/v1/albums/{album_id}"
+            headers = {"Authorization": f"Bearer {self.bot.access_token}"}
+            response = requests.get(album_url, headers=headers)
+            if response.status_code == 200:
+                album = response.json()
+
+                embed = discord.Embed(
+                    title = "Album Queued",
+                    description = f"**{album['name']}** by **{album['artists'][0]['name']}**\n` {len(album['tracks']['items'])} ` tracks\n\nQueued by: {interaction.user.mention}",
+                    color=BOT_COLOR
+                )
+                embed.set_thumbnail(url=album['images'][0]['url'])
+                embed.set_footer(text=datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%d %H:%M:%S')+" UTC")
+                await interaction.response.send_message(embed=embed)
+
+                tracks = await CustomSource.load_album(self, interaction.user, album)
+                for track in tracks['tracks']:
+                    player.add(requester=interaction.user, track=track)
+
+        if "open.spotify.com/track" in query:
+            track_id = query.split("track/")[1]
+            track_url = f"https://api.spotify.com/v1/tracks/{track_id}"
+            headers = {"Authorization": f"Bearer {self.bot.access_token}"}
+            response = requests.get(track_url, headers=headers)
+            if response.status_code == 200:
+                track = response.json()
+
+                embed = discord.Embed(
+                    title = "Track Queued",
+                    description = f"**{track['name']}** by **{track['artists'][0]['name']}**\n\nQueued by: {interaction.user.mention}",
+                    color=BOT_COLOR
+                )
+                embed.set_thumbnail(url=track['album']['images'][0]['url'])
+                embed.set_footer(text=datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%d %H:%M:%S')+" UTC")
+                await interaction.response.send_message(embed=embed)
+
+                track_ = await CustomSource.load_item(self, interaction.user, track)
+                player.add(requester=interaction.user, track=track_.tracks[0])
+
+        if 'open.spotify.com/artists' in query:
+            return await interaction.response.send_message("Sorry, I can't play artists. You must provide a song/album/playlist.", ephemeral=True)
+
+        if 'open.spotify.com' in query:
+            if not player.is_playing:
+                await player.play()
+            return
+
+        # Now begins the soundcloud section, this can be just like the youtube section
         if not url_rx.match(query):
             query = f'ytsearch:{query}'
         results = await player.node.get_tracks(query)
 
-        if results.load_type == LoadType.EMPTY:
+        # Below is for YouTube search, which is the default and is used when no link is provided
+        if not url_rx.match(query) and not results or not results['tracks']:
+            query = f'scsearch:{query}'
+        results = await player.node.get_tracks(query)
+
+        if not results or not results['tracks']:
             return await interaction.response.send_message('Nothing found!', ephemeral=True)
 
         embed = discord.Embed(color=BOT_COLOR)
 
-        if results.load_type == LoadType.PLAYLIST:
-            tracks = results.tracks
+        if results['loadType'] == 'PLAYLIST_LOADED':
+            tracks = results['tracks']
 
             for track in tracks:
-                player.add(requester=interaction.user, track=track)
+                track_ = lavalink.AudioTrack(track, interaction.user.id, extra=f"https://img.youtube.com/vi/{track['info']['identifier']}/hqdefault.jpg")
+                player.add(requester=interaction.user, track=track_)
 
-            embed.title = 'Songs Queued!'
-            embed.description = f"**{results.playlist_info.name}**\n` {len(tracks)} ` tracks\n\nQueued by: {interaction.user.mention}"
+            embed.title = 'Playlist Queued!'
+            embed.description = f"**{results['playlistInfo']['name']}**\n` {len(tracks)} ` tracks\n\nQueued by: {interaction.user.mention}"
             embed.set_footer(text=datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%d %H:%M:%S')+" UTC")
         else:
-            track = results.tracks[0]
-
+            track = results['tracks'][0]
             embed.title = 'Track Queued'
-            embed.description = f"**{track.title}** by **{track.author}**\n\nQueued by: {interaction.user.mention}"
-            embed.set_thumbnail(url=track.artwork_url)
+            embed.description = f"**{track['info']['title']}** by **{track['info']['author']}**\n\nQueued by: {interaction.user.mention}"
+            embed.set_thumbnail(url=f"https://img.youtube.com/vi/{track['info']['identifier']}/hqdefault.jpg")
             embed.set_footer(text=datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%d %H:%M:%S')+" UTC")
 
-            player.add(requester=interaction.user, track=track)
+            track_ = lavalink.AudioTrack(track, interaction.user.id, recommended=True, extra=f"https://img.youtube.com/vi/{track['info']['identifier']}/hqdefault.jpg")
+            player.add(requester=interaction.user, track=track_)
 
         await interaction.response.send_message(embed=embed)
 
@@ -223,7 +294,7 @@ class Music(commands.Cog):
         await guild.voice_client.disconnect(force=True)
         embed = discord.Embed(
             title="Queue Cleared and Music Stopped",
-            description=f"Thank you for using Guava :wave:\n\nIssued by: {interaction.user.mention}",
+            description=f"Thank you for using Aqua Bot :wave:\n\nIssued by: {interaction.user.mention}",
             color=BOT_COLOR
         )
         embed.set_footer(text=datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%d %H:%M:%S')+" UTC")
@@ -253,7 +324,7 @@ class Music(commands.Cog):
         embed = discord.Embed(
             title="Queue Cleared",
             description = f"The queue has been cleared of all songs!\n\nIssued by: {interaction.user.mention}",
-            color=BOT_COLOR
+            color=0x0088a9
         )
         embed.set_footer(text=datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%d %H:%M:%S')+" UTC")
         await interaction.response.send_message(embed=embed)
@@ -282,7 +353,7 @@ class Music(commands.Cog):
         if not player.is_playing:
             embed = discord.Embed(
                 title="Track Skipped",
-                description=f"The queue is now empty, so I have left the voice channel. Thank you for using Guava.\n\nIssued by: {interaction.user.mention}",
+                description=f"The queue is now empty, so I have left the voice channel. Thank you for using Aqua Bot.\n\nIssued by: {interaction.user.mention}",
                 color=BOT_COLOR
             )
             embed.set_footer(text=datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%d %H:%M:%S')+" UTC")
@@ -293,7 +364,7 @@ class Music(commands.Cog):
             description = f"**Now Playing: [{player.current.title}]({player.current.uri})**\n\nQueued by: {player.current.requester.mention}",
             color=BOT_COLOR
         )
-        embed.set_thumbnail(url=player.current.artwork_url)
+        embed.set_thumbnail(url=player.current.extra['extra'])
         embed.set_footer(text=datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%d %H:%M:%S')+" UTC")
         await interaction.response.send_message(embed=embed)
 
@@ -323,7 +394,7 @@ class Music(commands.Cog):
             description = f"**[{player.current.title}]({player.current.uri})**\n\nQueued by: {player.current.requester.mention}",
             color=BOT_COLOR
         )
-        embed.set_thumbnail(url=player.current.artwork_url)
+        embed.set_thumbnail(url=player.current.extra['extra'])
         embed.set_footer(text=datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%d %H:%M:%S')+" UTC")
         await interaction.response.send_message(embed=embed)
 
@@ -353,7 +424,7 @@ class Music(commands.Cog):
             description=f"**[{player.current.title}]({player.current.uri})**\n\nQueued by: {player.current.requester.mention}",
             color=BOT_COLOR
         )
-        embed.set_thumbnail(url=player.current.artwork_url)
+        embed.set_thumbnail(url=player.current.extra['extra'])
         embed.set_footer(text=datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%d %H:%M:%S')+" UTC")
         await interaction.response.send_message(embed=embed)
 
@@ -435,7 +506,7 @@ class Music(commands.Cog):
             description=f"**[{player.current.title}]({player.current.uri})**\n{f'` {time_in}/{total_duration} `'}\n\nQueued by: {player.current.requester.mention}",
             color=BOT_COLOR
         )
-        embed.set_thumbnail(url=player.current.artwork_url)
+        embed.set_thumbnail(url=player.current.extra['extra'])
         embed.set_footer(text=datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%d %H:%M:%S')+" UTC")
         await interaction.response.send_message(embed=embed)
 
@@ -474,7 +545,7 @@ class Music(commands.Cog):
             description=f"**Song Removed - [{removed_title}]({removed_url})**\n\nIssued by: {interaction.user.mention}",
             color=BOT_COLOR
         )
-        embed.set_thumbnail(url=player.current.artwork_url)
+        embed.set_thumbnail(url=player.current.extra['extra'])
         embed.set_footer(text=datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%d %H:%M:%S')+" UTC")
         await interaction.response.send_message(embed=embed)
 
